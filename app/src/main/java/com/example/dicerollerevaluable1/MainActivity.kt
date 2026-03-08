1b6f5299d866259f784e403c6bd5c52c306bcb9e
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-08 15:00:20 +0100

    Inflamos el main

--- app/src/main/java/com/example/dicerollerevaluable1/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerevaluable1/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dicerollerevaluable1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var dadoNumero = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
