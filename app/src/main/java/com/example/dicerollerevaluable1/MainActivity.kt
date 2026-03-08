a6696aa3df4f799d96856eb24e27ab13e8202513
Hector Ramos Gomez <hectorramosgomez717@gmail.com>
2026-03-08 15:06:13 +0100

    Funcion asignar foto para cada numero

--- app/src/main/java/com/example/dicerollerevaluable1/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerevaluable1/MainActivity.kt
@@ -21,4 +21,15 @@ class MainActivity : AppCompatActivity() {
     private fun generarNumeroRandom(): Int {
         return (1..6).random()
     }
+
+    private fun asignarFoto(dadoNumero: Int) {
+        var fotos = when(dadoNumero) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
 }
